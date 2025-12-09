ed61092075d36694297e3d3f57dfaadbb5a1f816
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-09 21:58:31 +0100

    updates to create.blade.php

--- resources/views/pages/purchases/suppliers/create.blade.php
+++ resources/views/pages/purchases/suppliers/create.blade.php
@@ -1,23 +1,45 @@
-<h2>Add Supplier</h2>
+@extends('layouts.app')
 
-<form action="{{ route('suppliers.store') }}" method="POST">
-    @csrf
+@section('content')
+<div class="container mt-4">
 
-    @if(isset($returnTo))
-        <input type="hidden" name="return_to" value="{{ $returnTo }}">
-    @endif
+    <h2>Add Supplier</h2>
 
-    <label>Name</label>
-    <input type="text" name="name" required>
+    <div class="card mt-3">
+        <div class="card-body">
 
-    <label>Email</label>
-    <input type="email" name="email">
+            <form action="{{ route('suppliers.store') }}" method="POST">
+                @csrf
 
-    <label>Phone</label>
-    <input type="text" name="phone">
+                @if(isset($returnTo))
+                    <input type="hidden" name="return_to" value="{{ $returnTo }}">
+                @endif
 
-    <label>Address</label>
-    <textarea name="address"></textarea>
+                <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input type="text" name="name" class="form-control" required>
+                </div>
 
-    <button type="submit">Save</button>
-</form>
+                <div class="mb-3">
+                    <label class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Phone</label>
+                    <input type="text" name="phone" class="form-control">
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label">Address</label>
+                    <textarea name="address" class="form-control"></textarea>
+                </div>
+
+                <button class="btn btn-success">Save</button>
+            </form>
+
+        </div>
+    </div>
+
+</div>
+@endsection
