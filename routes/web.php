92c81a4bceaeed43694e45fb7752b71594321e31
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-09 22:46:01 +0100

    updates to web.php

--- routes/web.php
+++ routes/web.php
@@ -3,41 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\PurchaseRequestController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
-Route::get('/', function () {
-    return redirect()->route('dashboard');
+// Home route (optional redirect to purchase requests)
+Route::get('/', function() {
+    return redirect()->route('purchase-requests.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// ---------------- SUPPLIERS ----------------
 
-/*
-| Suppliers
-*/
+// Standard resource routes
 Route::resource('suppliers', SupplierController::class);
 
-/*
-| Purchase Requests
-*/
+// Quick store for modal AJAX (PurchaseRequest form)
+Route::post('suppliers/quick-store', [SupplierController::class, 'storeQuick'])
+    ->name('suppliers.storeQuick');
+
+// ---------------- PURCHASE REQUESTS ----------------
+
+// Standard resource routes
 Route::resource('purchase-requests', PurchaseRequestController::class);
-
-/*
-| AJAX endpoint to create supplier quickly from modal
-*/
-Route::post('/suppliers-quick', [SupplierController::class, 'storeQuick'])->name('suppliers.storeQuick');
-
-
-
-
-Route::post('suppliers/quick-store', [SupplierController::class,'storeQuick'])->name('suppliers.storeQuick');
