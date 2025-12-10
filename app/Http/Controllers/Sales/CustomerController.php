04b0f3c212346d8ca11e671662a10e72e2a5e778
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-10 22:04:29 +0100

    updates to CustomerController.php

--- app/Http/Controllers/Sales/CustomerController.php
+++ app/Http/Controllers/Sales/CustomerController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Sales;
 
 use App\Models\Models\Sales\Customer;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CustomerController extends Controller
 {
