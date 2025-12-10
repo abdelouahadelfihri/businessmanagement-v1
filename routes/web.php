cfc7d386cefc2077300f2621a41529b0a2178fb1
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-10 21:51:36 +0100

    updates to web.php

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Purchases\SupplierController;
 use App\Http\Controllers\Purchases\PurchaseRequestController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
-// Home route (optional redirect to purchase requests)
-Route::get('/', function() {
+// Redirect home to purchase request list
+Route::get('/', function () {
     return redirect()->route('purchase-requests.index');
 });
 
 // ---------------- SUPPLIERS ----------------
-
-// Standard resource routes
 Route::resource('suppliers', SupplierController::class);
-
-// Quick store for modal AJAX (PurchaseRequest form)
 Route::post('suppliers/quick-store', [SupplierController::class, 'storeQuick'])
     ->name('suppliers.storeQuick');
 
 // ---------------- PURCHASE REQUESTS ----------------
-
-// Standard resource routes
 Route::resource('purchase-requests', PurchaseRequestController::class);
