b590a12d9c083f95e9843e87473c059add1be91b
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-13 16:53:25 +0100

    updates web.php

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,22 @@ use App\Http\Controllers\Purchases\PurchaseOrderController;
 use App\Http\Controllers\Sales\SalesOrderController;
 use App\Http\Controllers\MasterData\InventoryController;
 use App\Http\Controllers\MasterData\ProductController;
+use App\Http\Controllers\MasterData\CategoryController;
+use App\Http\Controllers\MasterData\UnitController;
+use App\Http\Controllers\MasterData\WarehouseController;
+
 
 Route::resource('suppliers', SupplierController::class);
 Route::resource('purchasesrequests', PurchaseRequestController::class);
 Route::resource('purchasesorders', PurchaseOrderController::class);
 Route::resource('salesorders', SalesOrderController::class);
+
+Route::resource('categories', CategoryController::class);
 Route::resource('inventories', InventoryController::class);
 Route::resource('products', ProductController::class);
+Route::resource('units', UnitController::class);
+Route::resource('warehouses', WarehouseController::class);
+
 Route::get('/', function () {
     return view('dashboard'); // or the correct dashboard view file
 })->name('dashboard');
