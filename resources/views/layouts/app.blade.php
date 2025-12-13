238ca8631b6b80f21ea54638c510a191523e3c07
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-13 11:57:28 +0100

    updates to app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@
         body {
             min-height: 100vh;
         }
+
         .sidebar {
             min-width: 220px;
             max-width: 220px;
@@ -17,99 +19,118 @@
             color: #fff;
             transition: all 0.3s;
         }
+
         .sidebar a {
             color: #fff;
             text-decoration: none;
         }
+
         .sidebar a:hover {
             background-color: #495057;
             color: #fff;
         }
+
         .sidebar .submenu a {
             padding-left: 30px;
         }
+
         .content {
             padding: 20px;
             flex-grow: 1;
         }
     </style>
 </head>
+
 <body>
-<div class="d-flex">
-    <!-- Sidebar -->
-    <nav class="sidebar d-flex flex-column p-3">
-        <h4 class="text-center mb-4">BMS</h4>
-        <ul class="nav nav-pills flex-column mb-auto">
-            <!-- Purchases Menu -->
-            <li>
-                <a class="nav-link text-white" data-bs-toggle="collapse" href="#purchasesMenu" role="button" aria-expanded="false" aria-controls="purchasesMenu">
-                    Purchases
-                </a>
-                <div class="collapse" id="purchasesMenu">
-                    <ul class="nav flex-column submenu">
-                        <li><a class="nav-link text-white" data-bs-toggle="collapse" href="#purchaseRequestsMenu">Purchase Requests</a>
-                            <div class="collapse" id="purchaseRequestsMenu">
-                                <ul class="nav flex-column submenu">
-                                    <li><a class="nav-link text-white" href="{{ route('purchasesrequests.index') }}">List</a></li>
-                                    <li><a class="nav-link text-white" href="{{ route('purchasesrequests.create') }}">Add</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li><a class="nav-link text-white" data-bs-toggle="collapse" href="#purchaseOrdersMenu">Purchase Orders</a>
-                            <div class="collapse" id="purchaseOrdersMenu">
-                                <ul class="nav flex-column submenu">
-                                    <li><a class="nav-link text-white" href="{{ route('purchasesorders.list') }}">List</a></li>
-                                    <li><a class="nav-link text-white" href="{{ route('purchasesorders.add') }}">Add</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+    <div class="d-flex">
+        <!-- Sidebar -->
+        <nav class="sidebar d-flex flex-column p-3">
+            <h4 class="text-center mb-4">BMS</h4>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <!-- Purchases Menu -->
+                <li>
+                    <a class="nav-link text-white" data-bs-toggle="collapse" href="#purchasesMenu" role="button"
+                        aria-expanded="false" aria-controls="purchasesMenu">
+                        Purchases
+                    </a>
+                    <div class="collapse" id="purchasesMenu">
+                        <ul class="nav flex-column submenu">
+                            <li><a class="nav-link text-white" data-bs-toggle="collapse"
+                                    href="#purchaseRequestsMenu">Purchase Requests</a>
+                                <div class="collapse" id="purchaseRequestsMenu">
+                                    <ul class="nav flex-column submenu">
+                                        <li><a class="nav-link text-white"
+                                                href="{{ route('purchasesrequests.index') }}">List</a></li>
+                                        <li><a class="nav-link text-white"
+                                                href="{{ route('purchasesrequests.create') }}">Add</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><a class="nav-link text-white" data-bs-toggle="collapse"
+                                    href="#purchaseOrdersMenu">Purchase Orders</a>
+                                <div class="collapse" id="purchaseOrdersMenu">
+                                    <ul class="nav flex-column submenu">
+                                        <li><a class="nav-link text-white"
+                                                href="{{ route('purchasesorders.list') }}">List</a></li>
+                                        <li><a class="nav-link text-white"
+                                                href="{{ route('purchasesorders.add') }}">Add</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
 
-            <!-- Sales Menu -->
-            <li>
-                <a class="nav-link text-white" data-bs-toggle="collapse" href="#salesMenu">Sales</a>
-                <div class="collapse" id="salesMenu">
-                    <ul class="nav flex-column submenu">
-                        <li><a class="nav-link text-white" data-bs-toggle="collapse" href="#salesOrdersMenu">Orders</a>
-                            <div class="collapse" id="salesOrdersMenu">
-                                <ul class="nav flex-column submenu">
-                                    <li><a class="nav-link text-white" href="{{ route('salesorders.list') }}">List</a></li>
-                                    <li><a class="nav-link text-white" href="{{ route('salesorders.add') }}">Add</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+                <!-- Sales Menu -->
+                <li>
+                    <a class="nav-link text-white" data-bs-toggle="collapse" href="#salesMenu">Sales</a>
+                    <div class="collapse" id="salesMenu">
+                        <ul class="nav flex-column submenu">
+                            <li><a class="nav-link text-white" data-bs-toggle="collapse"
+                                    href="#salesOrdersMenu">Orders</a>
+                                <div class="collapse" id="salesOrdersMenu">
+                                    <ul class="nav flex-column submenu">
+                                        <li><a class="nav-link text-white"
+                                                href="{{ route('salesorders.list') }}">List</a></li>
+                                        <li><a class="nav-link text-white" href="{{ route('salesorders.add') }}">Add</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
 
-            <!-- Inventories Menu -->
-            <li>
-                <a class="nav-link text-white" data-bs-toggle="collapse" href="#inventoriesMenu">Inventories</a>
-                <div class="collapse" id="inventoriesMenu">
-                    <ul class="nav flex-column submenu">
-                        <li><a class="nav-link text-white" data-bs-toggle="collapse" href="#productsMenu">Products</a>
-                            <div class="collapse" id="productsMenu">
-                                <ul class="nav flex-column submenu">
-                                    <li><a class="nav-link text-white" href="{{ route('products.index') }}">List</a></li>
-                                    <li><a class="nav-link text-white" href="{{ route('products.create') }}">Add</a></li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </nav>
+                <!-- Inventories Menu -->
+                <li>
+                    <a class="nav-link text-white" data-bs-toggle="collapse" href="#inventoriesMenu">Inventories</a>
+                    <div class="collapse" id="inventoriesMenu">
+                        <ul class="nav flex-column submenu">
+                            <li><a class="nav-link text-white" data-bs-toggle="collapse"
+                                    href="#productsMenu">Products</a>
+                                <div class="collapse" id="productsMenu">
+                                    <ul class="nav flex-column submenu">
+                                        <li><a class="nav-link text-white" href="{{ route('products.index') }}">List</a>
+                                        </li>
+                                        <li><a class="nav-link text-white" href="{{ route('products.create') }}">Add</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </nav>
 
-    <!-- Main content -->
-    <div class="content">
-        @yield('content')
+        <!-- Main content -->
+        <div class="content">
+            @yield('content')
+        </div>
     </div>
-</div>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
