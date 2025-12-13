f3c4b111ca6a53e909510e43f93ca7140cd2d0e2
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-13 12:08:18 +0100

    updates to web.php

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Purchases\SupplierController;
 use App\Http\Controllers\Purchases\PurchaseRequestController;
 use App\Http\Controllers\Purchases\PurchaseOrderController;
+use App\Http\Controllers\Sales\SalesOrderController;
+use App\Http\Controllers\MasterData\InventoryController;
 
 Route::resource('suppliers', SupplierController::class);
 Route::resource('purchasesrequests', PurchaseRequestController::class);
 Route::resource('purchasesorders', PurchaseOrderController::class);
+Route::resource('salesorders', SalesOrderController::class);
+Route::resource('inventories', InventoryController::class);
 Route::get('/', function () {
     return view('dashboard'); // or the correct dashboard view file
 })->name('dashboard');
