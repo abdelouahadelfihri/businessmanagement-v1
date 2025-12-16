22b953772d4fe6ec6d9ea2b06aedae797dcf6394
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-16 22:35:29 +0100

    updates to app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -254,20 +254,20 @@
                                 </div>
                             </li>
                             <li>
-                                <a class="nav-link" data-bs-toggle="collapse" href="#purchaseRequestsMenu">
+                                <a class="nav-link" data-bs-toggle="collapse" href="#purchasesInvoicesMenu">
                                     <i class="bi bi-file-earmark-text"></i>
-                                    <span>Purchase Requests</span>
+                                    <span>Purchases Invoices</span>
                                 </a>
-                                <div class="collapse" id="purchaseRequestsMenu">
+                                <div class="collapse" id="purchasesInvoicesMenu">
                                     <ul class="nav flex-column submenu">
                                         <li>
-                                            <a class="nav-link" href="{{ route('purchasesrequests.index') }}">
+                                            <a class="nav-link" href="{{ route('purchasesinvoices.index') }}">
                                                 <i class="bi bi-list-ul"></i>
                                                 <span>List</span>
                                             </a>
                                         </li>
                                         <li>
-                                            <a class="nav-link" href="{{ route('purchasesrequests.create') }}">
+                                            <a class="nav-link" href="{{ route('purchasesinvoices.create') }}">
                                                 <i class="bi bi-plus-circle"></i>
                                                 <span>Add</span>
                                             </a>
