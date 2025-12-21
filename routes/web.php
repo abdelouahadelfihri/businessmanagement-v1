66cbd719d6075b82e63f818debde0fcf11a6bef9
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-21 16:08:53 +0100

    updates to web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use App\Http\Controllers\Sales\DeliveryController;
 use App\Http\Controllers\Sales\SaleInvoiceController;
 use App\Http\Controllers\Sales\SaleReturnController;
 
-
 use App\Http\Controllers\MasterData\CustomerController;
 use App\Http\Controllers\MasterData\SupplierController;
 use App\Http\Controllers\MasterData\InventoryController;
@@ -24,27 +23,34 @@ use App\Http\Controllers\MasterData\WarehouseController;
 use App\Http\Controllers\MasterData\LocationController;
 use App\Http\Controllers\MasterData\StockMovementController;
 
-Route::resource('suppliers', SupplierController::class);
-Route::resource('purchasesrequests', PurchaseRequestController::class);
-Route::resource('purchasesorders', PurchaseOrderController::class);
-Route::resource('purchasesreceipts', PurchaseReceiptController::class);
-Route::resource('purchasesinvoices', PurchaseInvoiceController::class);
+Route::middleware(['web'])->group(function () {
 
-Route::resource('customers', CustomerController::class);
-Route::resource('salesquotations', SaleQuoteController::class);
-Route::resource('salesorders', SaleOrderController::class);
-Route::resource('salesdeliveries', DeliveryController::class);
-Route::resource('salesinvoices', SaleInvoiceController::class);
-Route::resource('salesreturns', SaleReturnController::class);
+    // Dashboard
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::resource('categories', CategoryController::class);
-Route::resource('inventories', InventoryController::class);
-Route::resource('products', ProductController::class);
-Route::resource('units', UnitController::class);
-Route::resource('warehouses', WarehouseController::class);
-Route::resource('locations', LocationController::class);
-Route::resource('stocksmovements', StockMovementController::class);
+    // Master Data
+    Route::resource('suppliers', SupplierController::class);
+    Route::resource('customers', CustomerController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('inventories', InventoryController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('units', UnitController::class);
+    Route::resource('warehouses', WarehouseController::class);
+    Route::resource('locations', LocationController::class);
+    Route::resource('stocksmovements', StockMovementController::class);
 
-Route::get('/', function () {
-    return view('dashboard'); // or the correct dashboard view file
-})->name('dashboard');
+    // Purchases
+    Route::resource('purchasesrequests', PurchaseRequestController::class);
+    Route::resource('purchasesorders', PurchaseOrderController::class);
+    Route::resource('purchasesreceipts', PurchaseReceiptController::class);
+    Route::resource('purchasesinvoices', PurchaseInvoiceController::class);
+
+    // Sales
+    Route::resource('salesquotations', SaleQuoteController::class);
+    Route::resource('salesorders', SaleOrderController::class);
+    Route::resource('salesdeliveries', DeliveryController::class);
+    Route::resource('salesinvoices', SaleInvoiceController::class);
+    Route::resource('salesreturns', SaleReturnController::class);
+});
