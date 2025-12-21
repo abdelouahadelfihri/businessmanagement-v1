a1f528a6a007b3a1435372d4eb932740060facf0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-21 11:48:34 +0100

    updates to create.blade.php

--- resources/views/purchasesrequests/create.blade.php
+++ resources/views/purchasesrequests/create.blade.php
@@ -41,8 +41,9 @@
                     {{-- Request date --}}
                     <div class="mb-3">
                         <label class="form-label">Request Date</label>
-                        <input type="date" name="request_date"
-                            value="{{ old('request_date', $formData['request_date'] ?? date('Y-m-d')) }}">
+
+                        <input type="date" name="request_date" class="form-control"
+                            value="{{ old('request_date', $form['request_date'] ?? '') }}" required>
                     </div>
 
                     <button class="btn btn-primary">Save</button>
