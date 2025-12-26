9d1d1fe292f01c3160673a27af779499fb2ad3e2
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-26 21:56:10 +0100

    updates to SaleReturnLine.php

--- app/Models/Sales/SaleReturn.php
+++ app/Models/Sales/SaleReturn.php
@@ -5,7 +5,7 @@ namespace App\Models\Sales;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SalesReturn extends Model
+class SaleReturn extends Model
 {
     use HasFactory;
 
--- app/Models/Sales/SaleReturnLine.php
+++ app/Models/Sales/SaleReturnLine.php
@@ -5,7 +5,7 @@ namespace App\Models\Sales;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SalesReturnLine extends Model
+class SaleReturnLine extends Model
 {
     use HasFactory;
 
