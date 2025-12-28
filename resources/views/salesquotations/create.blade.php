701cafaa7a383ce9865e44b52ac04f740cdcd372
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-28 15:18:47 +0100

    updates to create.blade.php

--- resources/views/salesquotations/create.blade.php
+++ resources/views/salesquotations/create.blade.php
@@ -16,7 +16,7 @@
 
         <div class="mb-3">
             <label>Quote Number</label>
-            <input name="quote_number" class="form-control" value="Auto-Generated" readonly>
+            <input name="quote_number" class="form-control" value="{{ $preview }}" readonly>
         </div>
         <div class="mb-3">
             <label>Date</label>
