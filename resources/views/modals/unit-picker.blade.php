55f01f06284223da0d220ffaa06c077de85e1af9
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-30 22:36:29 +0100

    updates to unit-picker.blade.php

--- resources/views/modals/unit-picker.blade.php
+++ resources/views/modals/unit-picker.blade.php
@@ -8,6 +8,22 @@
 
             <div class="modal-body">
 
+                {{-- ADD NEW UNIT --}}
+                <h6 class="fw-bold mb-2">Add New Unit</h6>
+
+                <div class="row g-2 mb-2">
+                    <div class="col-md-6">
+                        <input type="text" id="new_unit_name" class="form-control" placeholder="Unit Name">
+                    </div>
+                    <div class="col-md-6">
+                        <input type="email" id="new_unit_abbreviation" class="form-control" placeholder="Abbreviation">
+                    </div>
+                </div>
+
+                <button type="button" class="btn btn-success w-100" id="addUnitBtn">+ Add Unit</button>
+
+                <hr>
+
                 {{-- Search --}}
                 <div class="mb-2">
                     <input type="text" id="unitSearch" class="form-control" placeholder="Search units...">
@@ -17,22 +33,22 @@
                 <table class="table table-hover" id="unitTable">
                     <thead>
                         <tr>
+                            <th>Id</th>
                             <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
+                            <th>Abbreviation</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach(App\Models\MasterData\Unit::all() as $s)
+                        @foreach(App\Models\MasterData\Unit::all() as $unit)
                             <tr>
-                                <td>{{ $s->name }}</td>
-                                <td>{{ $s->email }}</td>
-                                <td>{{ $s->phone }}</td>
+                                <td>{{ $unit->id }}</td>
+                                <td>{{ $unit->name }}</td>
+                                <td>{{ $unit->abbreviation }}</td>
                                 <td>
                                     <button type="button" class="btn btn-sm btn-primary select-unit"
-                                        data-id="{{ $s->id }}" data-name="{{ $s->name }}" data-email="{{ $s->email }}"
-                                        data-phone="{{ $s->phone }}">
+                                        data-id="{{ $unit->id }}" data-name="{{ $unit->name }}"
+                                        data-abbreviation="{{ $unit->abbreviation }}">
                                         Select
                                     </button>
                                 </td>
@@ -40,31 +56,6 @@
                         @endforeach
                     </tbody>
                 </table>
-
-                <hr>
-
-                {{-- ADD NEW UNIT --}}
-                <h6 class="fw-bold mb-2">Add New Unit</h6>
-
-                <div class="row g-2 mb-2">
-                    <div class="col-md-6">
-                        <input type="text" id="new_unit_name" class="form-control" placeholder="Unit Name">
-                    </div>
-                    <div class="col-md-6">
-                        <input type="email" id="new_unit_email" class="form-control" placeholder="Email">
-                    </div>
-                </div>
-
-                <div class="row g-2 mb-2">
-                    <div class="col-md-6">
-                        <input type="text" id="new_unit_phone" class="form-control" placeholder="Phone">
-                    </div>
-                    <div class="col-md-6">
-                        <input type="text" id="new_unit_address" class="form-control" placeholder="Address">
-                    </div>
-                </div>
-
-                <button type="button" class="btn btn-success w-100" id="addUnitBtn">+ Add Unit</button>
             </div>
         </div>
     </div>
@@ -103,18 +94,18 @@
                     },
                     success: function (data) {
                         $('#unitTable tbody').append(`
-                                                    <tr>
-                                                        <td>${data.name}</td>
-                                                        <td>${data.email ?? ''}</td>
-                                                        <td>${data.phone ?? ''}</td>
-                                                        <td>
-                                                            <button type="button" class="btn btn-sm btn-primary select-unit"
-                                                                data-id="${data.id}" data-name="${data.name}" data-email="${data.email}" data-phone="${data.phone}">
-                                                                Select
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                `);
+                                                                                                <tr>
+                                                                                                    <td>${data.name}</td>
+                                                                                                    <td>${data.email ?? ''}</td>
+                                                                                                    <td>${data.phone ?? ''}</td>
+                                                                                                    <td>
+                                                                                                        <button type="button" class="btn btn-sm btn-primary select-unit"
+                                                                                                            data-id="${data.id}" data-name="${data.name}" data-email="${data.email}" data-phone="${data.phone}">
+                                                                                                            Select
+                                                                                                        </button>
+                                                                                                    </td>
+                                                                                                </tr>
+                                                                                            `);
 
                         // clear inputs
                         $('#new_unit_name').val('');
