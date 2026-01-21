9b5f7d03c58e59505fdfc0ac2123097b47b91b34
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-21 22:14:55 +0100

    updates to index.blade.php

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -35,13 +35,13 @@
                                         <td class="text-center">
                                             <div class="d-flex justify-content-center gap-1">
                                                 <!-- Edit button -->
-                                                <a href="{{ route('locations.edit', $req) }}" class="btn btn-sm btn-warning"
+                                                <a href="{{ route('locations.edit', $loc) }}" class="btn btn-sm btn-warning"
                                                     title="Edit">
                                                     <i class="bi bi-pencil-square"></i> Edit
                                                 </a>
 
                                                 <!-- Delete button -->
-                                                <form action="{{ route('locations.destroy', $req) }}" method="POST"
+                                                <form action="{{ route('locations.destroy', $loc) }}" method="POST"
                                                     onsubmit="return confirm('Are you sure you want to delete this request?');">
                                                     @csrf
                                                     @method('DELETE')
@@ -63,7 +63,7 @@
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 
             <div class="mt-3">
-                {{ $requests->withQueryString()->links() }}
+                {{ $locuests->withQueryString()->links() }}
             </div>
         @endif
     </div>
