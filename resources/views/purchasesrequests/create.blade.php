b38972f3927bd2f0ad91f35df5c7e4afc5ce183f
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-24 22:23:39 +0100

    updates to create.blade.php purchases requests

--- resources/views/purchasesrequests/create.blade.php
+++ resources/views/purchasesrequests/create.blade.php
@@ -4,7 +4,7 @@
     <div class="container mt-4">
         <div class="d-flex justify-content-between align-items-center mb-3">
             <h3>Create Purchase Request</h3>
-            <a href="{{ route('categories.index') }}" class="btn btn-secondary">
+            <a href="{{ route('purchasesrequests.index') }}" class="btn btn-secondary">
                 Back
             </a>
         </div>
@@ -49,11 +49,11 @@
 
         <!-- Actions -->
         <div class="d-flex justify-content-end">
-            <a href="{{ route('categories.index') }}" class="btn btn-outline-secondary me-2">
+            <a href="{{ route('purchasesrequests.index') }}" class="btn btn-outline-secondary me-2">
                 Cancel
             </a>
             <button type="submit" class="btn btn-primary">
-                Save Category
+                Save Purchase Request
             </button>
         </div>
     </form>
