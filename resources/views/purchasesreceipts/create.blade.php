7f6fee1f001a35989d643349f5787b5862623443
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-26 16:15:27 +0100

    updates to create.blade.php purchases receipts

--- resources/views/purchasesreceipts/create.blade.php
+++ resources/views/purchasesreceipts/create.blade.php
@@ -18,11 +18,12 @@
 
             <div class="mb-3">
                 <label>Supplier</label>
-                <select name="supplier_id" class="form-control">
-                    @foreach($suppliers as $supplier)
-                        <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
-                    @endforeach
-                </select>
+                <div class="input-group">
+                    <input type="hidden" name="supplier_id" id="supplier_id">
+                    <input type="text" id="supplier_name" class="form-control" readonly>
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
+                        data-bs-target="#supplierModal">Select</button>
+                </div>
             </div>
 
             <div class="mb-3">
@@ -46,67 +47,6 @@
             <button type="submit" class="btn btn-primary">Save Receipt</button>
         </form>
     </div>
-
-    <!-- Product Modal -->
-    <div class="modal fade" id="productModal" tabindex="-1">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5>Select Product</h5>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-bordered" id="product-list">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($products as $product)
-                                <tr>
-                                    <td>{{ $product->id }}</td>
-                                    <td>{{ $product->name }}</td>
-                                    <td>
-                                        <button type="button" class="btn btn-sm btn-success select-product"
-                                            data-id="{{ $product->id }}" data-name="{{ $product->name }}">Select</button>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script>
-        let lineCount = 0;
-
-        document.getElementById('add-line').addEventListener('click', function () {
-            $('#productModal').modal('show');
-        });
-
-        $(document).on('click', '.select-product', function () {
-            const id = $(this).data('id');
-            const name = $(this).data('name');
-            const row = `
-            <tr>
-                <td>
-                    <input type="hidden" name="lines[${lineCount}][product_id]" value="${id}">
-                    ${name}
-                </td>
-                <td><input type="number" name="lines[${lineCount}][quantity]" class="form-control" min="1" required></td>
-                <td><button type="button" class="btn btn-danger remove-line">Remove</button></td>
-            </tr>`;
-            $('#lines-table tbody').append(row);
-            lineCount++;
-            $('#productModal').modal('hide');
-        });
-
-        $(document).on('click', '.remove-line', function () {
-            $(this).closest('tr').remove();
-        });
-    </script>
+    {{-- PRODUCT MODAL --}}
+    @include('modals.product-picker')
 @endsection
