5d9e1c4e69c60e260a967725db922998da4c25e0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-26 18:53:19 +0100

    updates to edit.blade.php purchases receipts

--- resources/views/purchasesreceipts/edit.blade.php
+++ resources/views/purchasesreceipts/edit.blade.php
@@ -12,21 +12,11 @@
                 <label>Warehouse</label>
                 <select name="warehouse_id" class="form-control" required>
                     @foreach($warehouses as $warehouse)
-                        <option value="{{ $warehouse->id }}" @if($warehouse->id == $receipt->warehouse_id) selected @endif>
+                        <option value="{{ $warehouse->id }}" {{ $warehouse->id == $receipt->warehouse_id ? 'selected' : '' }}>
                             {{ $warehouse->name }}
                         </option>
                     @endforeach
                 </select>
-            </div>
-
-            <div class="mb-3">
-                <label>Supplier</label>
-                <div class="input-group">
-                    <input type="hidden" name="supplier_id" id="supplier_id">
-                    <input type="text" id="supplier_name" class="form-control" readonly>
-                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
-                        data-bs-target="#supplierModal">Select</button>
-                </div>
             </div>
 
             <div class="mb-3">
@@ -35,65 +25,91 @@
             </div>
 
             <h4>Products</h4>
+
             <table class="table" id="lines-table">
                 <thead>
                     <tr>
                         <th>Product</th>
-                        <th>Quantity</th>
-                        <th>Action</th>
+                        <th width="150">Quantity</th>
+                        <th width="100">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($receipt->lines as $i => $line)
                         <tr>
                             <td>
-                                <input type="hidden" name="lines[{{ $i }}][product_id]" value="{{ $line->product_id }}">
                                 {{ $line->product->name }}
+                                <input type="hidden" name="lines[{{ $i }}][product_id]" value="{{ $line->product_id }}">
                             </td>
-                            <td><input type="number" name="lines[{{ $i }}][quantity]" value="{{ $line->quantity }}"
-                                    class="form-control" min="1" required></td>
-                            <td><button type="button" class="btn btn-danger remove-line">Remove</button></td>
+                            <td>
+                                <input type="number" name="lines[{{ $i }}][quantity]" class="form-control" min="1"
+                                    value="{{ $line->quantity }}" required>
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-danger btn-sm remove-line">
+                                    Remove
+                                </button>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
 
-            <button type="button" class="btn btn-secondary" id="add-line">Add Product</button>
-            <button type="submit" class="btn btn-primary">Update Receipt</button>
+            <button type="button" class="btn btn-secondary" id="add-line">
+                Add Product
+            </button>
+
+            <button type="submit" class="btn btn-primary">
+                Update Receipt
+            </button>
         </form>
     </div>
 
-    {{-- PRODUCT MODAL --}}
     @include('modals.product-picker')
-    {{-- SUPPLIER MODAL --}}
-    @include('modals.supplier-picker')
+@endsection
 
+@push('scripts')
     <script>
-        let lineCount = {{ $receipt->lines->count() }};
+        let lineIndex = {{ $receipt->lines->count() }};
 
-        $(document).on('click', '.remove-line', function () {
-            $(this).closest('tr').remove();
-        });
-
-        document.getElementById('add-line').addEventListener('click', function () {
+        $('#add-line').on('click', function () {
             $('#productModal').modal('show');
         });
 
         $(document).on('click', '.select-product', function () {
             const id = $(this).data('id');
             const name = $(this).data('name');
+
             const row = `
-            <tr>
-                <td>
-                    <input type="hidden" name="lines[${lineCount}][product_id]" value="${id}">
-                    ${name}
-                </td>
-                <td><input type="number" name="lines[${lineCount}][quantity]" class="form-control" min="1" required></td>
-                <td><button type="button" class="btn btn-danger remove-line">Remove</button></td>
-            </tr>`;
+                    <tr>
+                        <td>
+                            ${name}
+                            <input type="hidden" name="lines[${lineIndex}][product_id]" value="${id}">
+                        </td>
+                        <td>
+                            <input type="number"
+                                   name="lines[${lineIndex}][quantity]"
+                                   class="form-control"
+                                   min="1"
+                                   value="1"
+                                   required>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-danger btn-sm remove-line">
+                                Remove
+                            </button>
+                        </td>
+                    </tr>
+                `;
+
             $('#lines-table tbody').append(row);
-            lineCount++;
+            lineIndex++;
+
             $('#productModal').modal('hide');
         });
+
+        $(document).on('click', '.remove-line', function () {
+            $(this).closest('tr').remove();
+        });
     </script>
-@endsection
+@endpush
