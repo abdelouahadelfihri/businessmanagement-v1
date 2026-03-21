2a560f4e2fa1b9f566a0c6aeecd7fca09e84ce87
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-03-21 17:38:13 +0000

    updates to Payment.php

--- app/Models/Finance/Payment.php
+++ app/Models/Finance/Payment.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'payable_id',
+        'payable_type',
+        'payment_date',
+        'amount',
+        'payment_method',
+        'reference',
+    ];
+
+    /**
+     * Polymorphic relation
+     * A payment belongs to either:
+     * - SalesInvoice
+     * - SupplierInvoice
+     */
+    public function payable()
+    {
+        return $this->morphTo();
+    }
 }
