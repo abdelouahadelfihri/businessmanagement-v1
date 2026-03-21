a67e74bd318579ef625b5fc75a048bd06995c64e
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-03-21 18:07:32 +0000

    updates to SalesInvoiceLine.php

--- app/Models/Sales/SalesInvoiceLine.php
+++ app/Models/Sales/SalesInvoiceLine.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SalesInvoiceLine extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'invoice_id',
+        'product_id',
+        'quantity',
+        'price',
+        'discount',
+        'total',
+    ];
+
+    public function invoice()
+    {
+        return $this->belongsTo(SalesInvoice::class, 'invoice_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
