f1b7727eb2c38be353c30d9d81d259a1d36735bb
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-03-21 18:09:17 +0000

    updates to SalesQuoteLine.php

--- app/Models/Sales/SalesQuoteLine.php
+++ app/Models/Sales/SalesQuoteLine.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SalesQuoteLine extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'quote_id',
+        'product_id',
+        'quantity',
+        'price',
+        'discount',
+        'total',
+    ];
+
+    public function quote()
+    {
+        return $this->belongsTo(SalesQuote::class, 'quote_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
