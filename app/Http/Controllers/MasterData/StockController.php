a451bacc6289f1f5b38171adbab853372bf02bbf
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-03-27 20:57:21 +0100

    updates to StockController.php

--- app/Http/Controllers/MasterData/StockController.php
+++ app/Http/Controllers/MasterData/StockController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\MasterData;
 
 use App\Models\MasterData\Product;
 use App\Http\Controllers\Controller;
-class StockMovementController extends Controller
+class StockController extends Controller
 {
     public function index()
     {
