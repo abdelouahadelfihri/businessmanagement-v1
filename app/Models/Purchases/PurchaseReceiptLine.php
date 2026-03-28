8ce93bc261333754fb2408393a024a98a9033a8c
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-03-28 16:29:30 +0100

    updates to PurchaseReceiptLine.php

--- app/Models/Purchases/PurchaseReceiptLine.php
+++ app/Models/Purchases/PurchaseReceiptLine.php
@@ -3,20 +3,20 @@
 namespace App\Models\Purchases;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Purchases\PurchaseReceipt;
 use App\Models\MasterData\Product;
 
 class PurchaseReceiptLine extends Model
 {
     protected $fillable = [
-        'receipt_id',
+        'purchase_receipt_id',
         'product_id',
         'quantity',
+        'unit_price',
     ];
 
     public function receipt()
     {
-        return $this->belongsTo(PurchaseReceipt::class, 'receipt_id');
+        return $this->belongsTo(PurchaseReceipt::class, 'purchase_receipt_id');
     }
 
     public function product()
