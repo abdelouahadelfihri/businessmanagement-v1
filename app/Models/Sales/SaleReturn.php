dfd3f713552b9c18780eaa16d454216ec18ce175
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-03-28 16:52:03 +0100

    updates to SaleReturn.php

--- app/Models/Sales/SaleReturn.php
+++ app/Models/Sales/SaleReturn.php
@@ -4,6 +4,8 @@ namespace App\Models\Sales;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\MasterData\Warehouse;
+use App\Models\MasterData\StockMovement;
 
 class SaleReturn extends Model
 {
@@ -14,6 +16,7 @@ class SaleReturn extends Model
         'customer_id',
         'return_number',
         'date',
+        'warehouse_id',
         'reason',
         'subtotal',
         'tax',
@@ -29,5 +32,20 @@ class SaleReturn extends Model
     public function salesOrder()
     {
         return $this->belongsTo(\App\Models\Sales\SaleOrder::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+
+    public function lines()
+    {
+        return $this->hasMany(SaleReturnLine::class, 'sale_return_id');
+    }
+
+    public function stockMovements()
+    {
+        return $this->morphMany(StockMovement::class, 'source');
     }
 }
