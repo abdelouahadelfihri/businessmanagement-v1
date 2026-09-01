5ed3fbe0ac0983c5a5dc0b569361c443a38c6d64
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-09-01 21:03:10 +0100

    updates to PurchaseOrderController.php

--- app/Http/Controllers/Purchases/PurchaseOrderController.php
+++ app/Http/Controllers/Purchases/PurchaseOrderController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Purchases;
 
 use Illuminate\Http\Request;
 use App\Models\Purchases\PurchaseOrder;
 use App\Models\Purchases\PurchaseRequest;
+use App\Http\Controllers\Controller;
 
 class PurchaseOrderController extends Controller
 {
